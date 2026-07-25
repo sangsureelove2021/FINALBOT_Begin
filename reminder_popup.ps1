85b3aaddc075f0408dc4a36dceabd81d79c079fc
sangsureelove2021 <globalud003@gmail.com>
2026-07-25 14:45:56 +0700

    NEW

--- reminder_popup.ps1
+++
@@ -1,5 +0,0 @@
-while ($true) {
-    Start-Sleep -Seconds 1200
-    Add-Type -AssemblyName PresentationFramework
-    [System.Windows.MessageBox]::Show('วินัย Ai', 'AI Discipline Reminder', 'OK', 'Warning')
-}
